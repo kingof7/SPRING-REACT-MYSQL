f695d6f20d4923acc8e1b7c3ff45e50b63d56630
jhlee <vlfl0@naver.com>
2024-09-07 19:19:11 +0900

    npx create-app react and springboot & JPA & add a datasource MySQL Connection

---
+++ board-backend/src/main/java/kr/co/sorin/board_backend/BoardBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sorin.board_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardBackendApplication.class, args);
+	}
+
+}
