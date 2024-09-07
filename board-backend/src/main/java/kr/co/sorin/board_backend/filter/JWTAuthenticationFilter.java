916156e3c04d3bb54ad30877ebe21af77650eea1
jhlee <vlfl0@naver.com>
2024-09-07 23:02:40 +0900

    9. Spring Security (Filter, Config)

---
+++ board-backend/src/main/java/kr/co/sorin/board_backend/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,86 @@
+package kr.co.sorin.board_backend.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import kr.co.sorin.board_backend.provider.JwtProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = parseBearerToken(request);
+
+        try {
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if (email == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            // context에 등록
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null,
+                    AuthorityUtils.NO_AUTHORITIES);
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // 웹 인증정보 세부정보
+                                                                                                        // 셋팅
+
+            SecurityContext SecurityContext = SecurityContextHolder.createEmptyContext();
+            SecurityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(SecurityContext); // 외부에서 사용 가능
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response); // 다음 필터로 request를 넘긴다.
+
+    }
+
+    private String parseBearerToken(HttpServletRequest request) {
+
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+
+        if (!hasAuthorization)
+            return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if (!isBearer)
+            return null;
+
+        String token = authorization.substring(7);
+
+        return token;
+
+    }
+
+}
---
+++ board-backend/src/main/java/kr/co/sorin/board_backend/provider/JwtProvider.java
@@ -0,0 +1,44 @@
+package kr.co.sorin.board_backend.provider;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+
+    private String secretKey = "S3cr3tK3y";
+
+    public String create(String email) {
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, secretKey) // 수정: SignatureAlgorithm.ES256 ->
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredDate)
+                .compact();
+
+        return jwt;
+    }
+
+    public String validate(String jwt) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(jwt)
+                    .getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return claims.getSubject();
+    }
+}
