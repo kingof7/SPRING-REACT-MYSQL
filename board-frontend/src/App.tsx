eb069df1010a22c65d0e2dc67e9cf04821200f25
jhlee <vlfl0@naver.com>
2024-09-16 00:50:19 +0900

    16. 컴포넌트 생성 (Input Box) B

--- board-frontend/src/App.tsx
+++ board-frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputBox from "components/InputBox";
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
+
   return (
     <>
-      <InputBox />
+      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={false} setValue={setValue} message='error message' />
     </>
   );
 }
--- board-frontend/src/components/InputBox/index.tsx
+++ board-frontend/src/components/InputBox/index.tsx
@@ -1,24 +1,58 @@
-import React, { forwardRef } from 'react'
+import React, { ChangeEvent, forwardRef, SetStateAction, KeyboardEvent } from 'react'
 import './style.css'
 
-interface Props {
+//          interface: Input Box 컴포넌트 Properties           //
+interface Props { // 외부에서 받아오는 변수들 정의함
+    label: string;
+    type: 'text' | 'password'; // literal type
+    placeholder: string;
+    value: string;
+    setValue: React.Dispatch<SetStateAction<string>>; // App.tsx의 usetState에서 가져옴
+    error: boolean;
 
+    icon?: string; // 필수가 아님
+    onButtonClick?: () => void; // 필수가 아님
+
+    message?: string;
+
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
+//          component: Input Box 컴포넌트           //
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => { // input element를 컨트롤 하기위해 forwardRef를 사용한다.
 
-const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+    //          state: properties(variable)                  //
+    const { label, type, placeholder, value, error, icon, message } = props;    
+    
+    //          state: properties(function)                  //
+    const { setValue, onButtonClick, onKeyDown } = props;
+    
+    //          event handler: input 값 변경 이벤트 함수 처리          //
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setValue(value);
+    }
 
+    //          event handler: key 이벤트 함수 처리          //
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(!onKeyDown) return;
+        onKeyDown(event);
+    }
+
+    //          render: Input Box 컴포넌트                          //
     return(
         <div className='inputbox'>
-            <div className='inputbox-label'>{'비밀번호 *'}</div>
-            <div className='inputbox-container'>
-                <input className='input' />
-                <div className='icon-button'>
-                    <div className='icon eye-light-off-icon'></div>
-                </div>
+            <div className='inputbox-label'>{label}</div>
+            <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler} />
+                {onButtonClick !== undefined && (
+                    <div className='icon-button'>
+                        {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+                    </div>
+                )}
             </div>
-            <div className='inputbox-message'>{'비밀번호는 8자 이상 입력해주세요.'}</div>
+            {message !== undefined && (<div className='inputbox-message'>{message}</div>)}
         </div>
-    )
+    );
 
 });
 
