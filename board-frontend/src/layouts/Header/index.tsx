4ff656a0cf0ef356182954065e02e88025315a3e
kingof7 <vlfl0@naver.com>
2024-09-17 23:22:11 +0900

    21. Header 레이아웃 작성 C

--- board-frontend/src/layouts/Header/index.tsx
+++ board-frontend/src/layouts/Header/index.tsx
@@ -1,18 +1,34 @@
-import { ChangeEvent, useRef, useState, KeyboardEvent, useEffect } from "react";
-import "./style.css";
-import { useNavigate, useParams } from "react-router-dom";
-import { AUTH_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from "constant";
-import { useCookies } from "react-cookie";
-import { useLoginUserStore } from "stores";
+import { ChangeEvent, useRef, useState, KeyboardEvent, useEffect } from 'react';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
+import { useCookies } from 'react-cookie';
+import { useBoardStore, useLoginUserStore } from 'stores';
+import './style.css';
 
 //          component: Header layout            //
 export default function Header() {
   //          state: 로그인 유저 상태              //
   const { loginUser, setLoginUser, resetLoginUser } = useLoginUserStore();
+  //          state: path name 상태              //
+  const { pathname } = useLocation();
   //          state: cookie 상태                //
   const [cookies, setCookies] = useCookies();
   //          state: 로그인 상태      //
   const [isLogin, setLogin] = useState<boolean>(false);
+  //          state: 인증 페이지 상태       //
+  const [isAuthPage, setAuthPage] = useState<boolean>(false);
+  //          state: 메인 페이지 상태       //
+  const [isMainPage, setMainPage] = useState<boolean>(false);
+  //          state: 검색 페이지 상태       //
+  const [isSearchPage, setSearchPage] = useState<boolean>(false);
+  //          state: 게시물 상세 페이지 상태       //
+  const [isBoardDetailPage, setBoardDetailPage] = useState<boolean>(false);
+  //          state: 게시물 작성 페이지 상태       //
+  const [isBoardWritePage, setBoardWritePage] = useState<boolean>(false);
+  //          state: 게시물 수정 페이지 상태       //
+  const [isBoardUpdatePage, setBoardUpdatePage] = useState<boolean>(false);
+  //          state: 유저 페이지 상태             //
+  const [isUserPage, setUserPage] = useState<boolean>(false);
 
   //          function: 네비게이트 함수            //
   const navigate = useNavigate();
@@ -31,7 +47,7 @@ export default function Header() {
     const [status, setStatus] = useState<boolean>(false);
 
     //        state: 검색어 상태                //
-    const [word, setWord] = useState<string>("");
+    const [word, setWord] = useState<string>('');
 
     //        state: 검색어 path variable 상태               //
     const { searchWord } = useParams();
@@ -48,7 +64,7 @@ export default function Header() {
     const onSearchWordKeyDownHandler = (
       event: KeyboardEvent<HTMLInputElement>
     ) => {
-      if (event.key !== "Enter") return; // 내보내다
+      if (event.key !== 'Enter') return; // 내보내다
       if (!searchButtonRef) return;
       searchButtonRef.current?.click();
     };
@@ -73,27 +89,27 @@ export default function Header() {
     if (!status)
       //        render: 검색 버튼 컴포넌트 렌더 (클릭 false 상태)     //
       return (
-        <div className="icon-button" onClick={onSearchButtonClickHandler}>
-          <div className="icon search-light-icon"></div>
+        <div className='icon-button' onClick={onSearchButtonClickHandler}>
+          <div className='icon search-light-icon'></div>
         </div>
       );
     //        render: 검색 버튼 컴포넌트 렌더 (클릭 true 상태)     //
     return (
-      <div className="header-search-input-box">
+      <div className='header-search-input-box'>
         <input
-          className="header-search-input"
-          type="text"
-          placeholder="검색어를 입력해주세요."
+          className='header-search-input'
+          type='text'
+          placeholder='검색어를 입력해주세요.'
           value={word}
           onChange={onSearchWordChangeHandler}
           onKeyDown={onSearchWordKeyDownHandler}
         />
         <div
           ref={searchButtonRef}
-          className="icon-button"
+          className='icon-button'
           onClick={onSearchButtonClickHandler}
         >
-          <div className="icon search-light-icon"></div>
+          <div className='icon search-light-icon'></div>
         </div>
       </div>
     );
@@ -125,38 +141,72 @@ export default function Header() {
     //          render: 로그아웃 버튼 컴포넌트 렌더링            //
     if (isLogin && userEmail === loginUser?.email)
       return (
-        <div className="white-button" onClick={onSignOutButtonClickHandler}>
-          {"로그아웃"}
+        <div className='white-button' onClick={onSignOutButtonClickHandler}>
+          {'로그아웃'}
         </div>
       );
-    //          render: 마이페이지 버튼 컴포넌트 렌더링            //
+    //          render: 마이페이지 버튼 컴포넌트 렌더링            //
     if (isLogin)
       return (
-        <div className="white-button" onClick={onMyPageButtonClickHandler}>
-          {"마이페이지"}
+        <div className='white-button' onClick={onMyPageButtonClickHandler}>
+          {'마이페이지'}
         </div>
       );
     //          render: 로그인 버튼 컴포넌트 렌더링            //
     return (
-      <div className="black-button" onClick={onSignInButtonClickHandler}>
-        {"로그인"}
+      <div className='black-button' onClick={onSignInButtonClickHandler}>
+        {'로그인'}
       </div>
     );
   };
 
+  //          component: 업로드 버튼 컴포넌트         //
+  const UploadButton = () => {
+    //        state: 게시물 상태                     //
+    const { title, content, boardImageFileList, resetBoard } = useBoardStore();
+    //        event handler: 업로드 버튼 클릭 이벤트 처리 함수      //
+    const onUploadButtonClickHandler = () => {
+
+    }
+    //        render: 업로드 버튼 컴포넌트 렌더링     //
+    if(title && content)
+    return <div className='disable-button' onClick={onUploadButtonClickHandler}>{'업로드'}</div>;
+    return <div className='disable-button'>{'업로드'}</div>;
+  };
+
+  //          effect: pathname이 변경될 때마다 실행될 함수          //
+  useEffect(() => {
+    const isAuthPage = pathname.startsWith(AUTH_PATH());
+    setAuthPage(isAuthPage);
+    const isMainPage = pathname.startsWith(MAIN_PATH());
+    setMainPage(isMainPage);
+    const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+    setSearchPage(isSearchPage);
+    const isBoardDetailPage = pathname.startsWith(BOARD_PATH() + '/' + BOARD_DETAIL_PATH(''));
+    setBoardDetailPage(isBoardDetailPage);
+    const isBoardWritePage = pathname.startsWith(BOARD_PATH() + '/' + BOARD_WRITE_PATH());
+    setBoardWritePage(isBoardWritePage);
+    const isBoardUpdatePage = pathname.startsWith(BOARD_PATH() + '/' + BOARD_UPDATE_PATH(''));
+    setBoardUpdatePage(isBoardUpdatePage);
+    const isUserPage = pathname.startsWith(USER_PATH(''));
+    setUserPage(isUserPage);
+
+  }, [pathname]);
+
   //          render: Header layout            //
   return (
-    <div id="header">
-      <div className="header-container">
-        <div className="header-left-box" onClick={onLogoClickHandler}>
-          <div className="icon-box">
-            <div className="icon logo-dark-icon"></div>
+    <div id='header'>
+      <div className='header-container'>
+        <div className='header-left-box' onClick={onLogoClickHandler}>
+          <div className='icon-box'>
+            <div className='icon logo-dark-icon'></div>
           </div>
-          <div className="header-logo">{"Bellflower's Board"}</div>
+          <div className='header-logo'>{'Bellflower\'s Board'}</div>
         </div>
-        <div className="header-right-box">
-          <SearchButton />
-          <MyPageButton />
+        <div className='header-right-box'>
+          {(isAuthPage || isMainPage || isSearchPage || isBoardDetailPage) && <SearchButton />}
+          {(isMainPage || isSearchPage || isBoardDetailPage || isUserPage) && <MyPageButton />}
+          {(isBoardWritePage || isBoardUpdatePage) && <UploadButton />}
         </div>
       </div>
     </div>
--- board-frontend/src/stores/index.ts
+++ board-frontend/src/stores/index.ts
@@ -1,5 +1,4 @@
-import useLoginUserStore from './login-user.store';
+import useBoardStore from "./board.store";
+import useLoginUserStore from "./login-user.store";
 
-export {
-    useLoginUserStore
-}
+export { useBoardStore, useLoginUserStore };
