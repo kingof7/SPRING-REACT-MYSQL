8caf2be3924add360c59de93f47723c2a2d0269f
kingof7 <vlfl0@naver.com>
2024-09-17 21:54:23 +0900

    c

--- board-frontend/src/layouts/Header/index.tsx
+++ board-frontend/src/layouts/Header/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
 //          component: Header layout            //
 export default function Header() {
-
-  //          render: Header layout            //  
+  //          render: Header layout            //
   return (
-    <div>Header</div>
-  )
+    <div id="header">
+      <div className="header-container">
+        <div className="header-left-box">
+          <div className='icon-box'></div>
+          <div className='header-logo'>{'Bellflower\'s Board'}</div>
+        </div>
+        <div className="header-right-box"></div>
+      </div>
+    </div>
+  );
 }
